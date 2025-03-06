d38f2fc92351fe206973d871d7d645538157f39d
Hager Atef <hageratefabdelfattah@gmail.com>
2025-03-06 12:33:16 +0200

    fix

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,18 +1,9 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRoutesConfig } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { provideServerRendering } from '@angular/platform-server';
 
-import { provideServerRenderingConfig } from '@angular/platform-server';
-
-export const config = {
-  providers: [
-    provideServerRenderingConfig({
-      renderMode: 'no-prerender', // ✅ This disables prerendering for dynamic routes
-    }),
-  ],
-};
 
 const serverConfig: ApplicationConfig = {
   providers: [
