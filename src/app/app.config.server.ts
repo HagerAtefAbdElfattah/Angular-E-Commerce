22174c5751b23329fa42fb68ce98ca39480ecb74
Hager Atef <hageratefabdelfattah@gmail.com>
2025-03-06 13:19:59 +0200

    fix

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,14 +1,13 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRoutesConfig } from '@angular/ssr';
+import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
-import { provideServerRendering } from '@angular/platform-server';
-
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRoutesConfig(serverRoutes)
+    provideServerRouting(serverRoutes)
   ]
 };
 
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,29 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'details/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return [{ id: '' }, { id: '' }]; 
+    }
+  },
+  {
+    path: 'orderDetails/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return [{ id: '' }, { id: '' }]; 
+    }
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return [{ id: '' }, { id: '' }]; 
+    }
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender 
   }
 ];
+
